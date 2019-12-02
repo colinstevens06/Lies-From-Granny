1aaef18132fabf69112ee00639ee812f89ff794f
Colin Stevens <colinstevens06@gmail.com>
2019-12-01 23:54:41 -0500

    trying to add transitions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,30 @@ var mainScreenDiv = $("#main-screen")
 var showsContentDiv = $("#shows-content")
 var showsCard = $("#showsCard")
 
+$(".nav-link").on("click", function(event) {
+   event.preventDefault();
+
+   const href = $(this).attr("href");
+   console.log(href);
+   
+   window.history.pushState(null, null, href);
+
+
+
+   $.ajax({
+      url: href,
+      success: function (data) {
+         $("section").fadeOut(250, function () {
+            const newPage = $(data).filter("section").html();
+
+            $("section").html(newPage)
+         })
+         $("section").fadeIn(250);
+      }
+   })
+
+})
+
 // function showMainScreen() {
 //    mainScreenDiv.css("display", "block");
 //    showsContentDiv.css("display", "none");
