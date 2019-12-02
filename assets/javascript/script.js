774ef5b58003d4b81a22796eab25e8650e2bd544
Colin Stevens <colinstevens06@gmail.com>
2019-12-01 23:27:05 -0500

    updating new shows page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,17 +2,17 @@ var mainScreenDiv = $("#main-screen")
 var showsContentDiv = $("#shows-content")
 var showsCard = $("#showsCard")
 
-function showMainScreen() {
-   mainScreenDiv.css("display", "block");
-   showsContentDiv.css("display", "none");
-}
+// function showMainScreen() {
+//    mainScreenDiv.css("display", "block");
+//    showsContentDiv.css("display", "none");
+// }
 
-function showShows() {
-   mainScreenDiv.css("display", "none");
-   showsContentDiv.css("display", "block");
-}
+// function showShows() {
+//    mainScreenDiv.css("display", "none");
+//    showsContentDiv.css("display", "block");
+// }
 
 
-$("#shows-nav").on("click", showShows)
-$("#home-nav").on("click", showMainScreen)
-$("#shows-card").on("click", showShows)
+// $("#shows-nav").on("click", showShows)
+// $("#home-nav").on("click", showMainScreen)
+// $("#shows-card").on("click", showShows)
