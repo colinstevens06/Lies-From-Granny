32101dfdf34104bd311a2d3ebc5e200c3a75d9a0
Colin Stevens <colinstevens06@gmail.com>
2019-12-15 22:17:24 -0500

    updated transitions in between pages

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,44 +1,43 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
    // **************** DECLARING VARIABLES HERE ****************
-   var sectionElement = $("section")
-   var windowWidth = window.innerWidth
-
+   var sectionElement = $("section");
+   var windowWidth = window.innerWidth;
 
    console.log(windowWidth);
-
 
    // **************** WRITING FUNCTIONS HERE ****************
    function showMainScreen(event) {
       event.preventDefault();
-      var goToURL = $(this).attr("data-url")
+      var goToURL = $(this).attr("data-url");
 
-      sectionElement.fadeOut(500)
-      setTimeout(function () { document.location.href = goToURL }, 500)
-         ;
-
+      sectionElement.fadeOut(500);
+      setTimeout(function() {
+         document.location.href = goToURL;
+      }, 500);
    }
 
-      function onLoad() {
-         sectionElement.fadeIn(1000)
+   function onLoad() {
+      sectionElement.fadeIn(1000);
 
-         if (windowWidth <= 400) {
-            switch ($(".main-image").attr("data-main-img")) {
-               case "shows":
-                  $(".main-image").css("display", "none");
+      if (windowWidth <= 400) {
+         switch ($(".main-image").attr("data-main-img")) {
+            case "shows":
+               $(".main-image").css("display", "none");
                break;
-               case "home":
-                     $(".main-image").attr("src", "./assets/images/home/Lies-From-Granny-Mobile.jpg");
-            }
+            case "home":
+               $(".main-image").attr(
+                  "src",
+                  "./assets/images/home/Lies-From-Granny-Mobile.jpg"
+               );
          }
-
       }
+   }
 
-      // **************** RUNNING FUNCTIONS HERE ****************
-      onLoad();
+   // **************** RUNNING FUNCTIONS HERE ****************
+   onLoad();
 
-      // **************** EVENT LISTENERS HERE ****************
-      $(".nav-btn").on("click", showMainScreen)
+   // **************** EVENT LISTENERS HERE ****************
+   $(".nav-btn").on("click", showMainScreen);
 
-      // closes out the document.ready
-   }); 
+   // closes out the document.ready
+});
