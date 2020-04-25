934cb50bbf72dcca38635f1970e37bcfe78a2303
Colin Stevens <colinstevens06@gmail.com>
2020-04-25 16:42:08 -0400

    Updates

---
+++ precache-manifest.fbd5b64d596700f19c51ef6043b24533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f40c91ab7aee5655781a2bc77d57c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4824e08e258e53a34299",
+    "url": "/static/js/2.e12a2b3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e12a2b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3c247a23e5ed5d81ed",
+    "url": "/static/js/main.b65a483c.chunk.js"
+  },
+  {
+    "revision": "acfa8cd31f3d0c78d724",
+    "url": "/static/js/runtime-main.25a895a8.js"
+  }
+]);
