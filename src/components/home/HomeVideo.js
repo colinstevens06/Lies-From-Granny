f8553ab548f9a6729a4215d38857045d01736b4c
Colin Stevens <colinstevens06@gmail.com>
2020-05-22 21:05:26 -0400

    updated typo rona2020

--- src/components/home/HomeVideo.js
+++ src/components/home/HomeVideo.js
@@ -8,7 +8,7 @@ export default function HomeVideo() {
           <h2 className="my-4 video-header">Videos</h2>
           <div className="home-video-container mb-4">
             <iframe src="https://www.youtube.com/embed/videoseries?list=PLWWWFDqFWHvR3x_Sf-E3DWfEnRGqmUpG7" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="video" className="video"></iframe>
-            <h2 className="mt-5 mb-4 video-header">#RONA 2020 Series</h2>
+            <h2 className="mt-5 mb-4 video-header">#RONA2020 Series</h2>
 
             <iframe src="https://www.youtube.com/embed/videoseries?list=PLWWWFDqFWHvRes-9N0xjfzI-5MI__gB_S" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="video" className="video"></iframe>
 
