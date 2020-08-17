6d7391b88cbda02bc61d6706fd0c4c4267b0b440
Colin Stevens <colinstevens06@gmail.com>
2020-08-16 21:07:24 -0400

    updated w/ renegade show

--- src/pages/Shows.js
+++ src/pages/Shows.js
@@ -23,6 +23,36 @@ export default function ShowsTest() {
 
             </div>
             <div className="container">
+              <h2 className="my-4">Upcoming Shows</h2>
+
+
+              <div className="show-card">
+
+                <div className="row">
+                  <div className="col-md-6 gig-image">
+                    <img src="../../assets/images/shows/Renegade-09-03-20.jpg"
+                      alt="The Renegade - Thurs Sept. 3, 2020" className="show-img img-fluid" />
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="gig-title">The Renegade</div>
+                    <div className="gig-deets">Sept. 3, 2020</div>
+                    <div>Rockin' the Renegade in Arlington. You can join us two different ways:</div>
+                    <ul>
+                      <li>Online via the Renegade's live streamt</li>
+                      <li>In person w/ a table reservation</li>
+                    </ul>
+                    <div>No matter how you join us, get ready for a rockin' evening w/ a bunch of your favorites.</div>
+
+
+                  </div>
+
+                </div>
+
+
+              </div>
+              <h2>Past Shows</h2>
+
               <ShowsContainer />
             </div>
             {/* <!-- closes body container --> */}
